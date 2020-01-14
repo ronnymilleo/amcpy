3fcc10f568da3174d6d305288a0631ad58bd57e7
Ronny Milléo <ronnysrmilleo@gmail.com>
2020-01-14 11:32:04 -0300

    Arquivo com funções e atualização no main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from functions import gmax, mean, std, meanofsquared
+import numpy as np
 
 print("Tests")
 
-n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-x = gmax(n)
-y = mean(n)
-z = std(n)
-a = meanofsquared(n)
+var = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+x = gmax(var)
+y = mean(var)
+z = std(var)
+a = meanofsquared(var)
 
 print(x)
 print(y)
