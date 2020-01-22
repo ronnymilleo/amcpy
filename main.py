6589d86205781572dcc3eb7ae5cf42dfcacd99ab
Ronny Milléo <ronnysrmilleo@gmail.com>
2020-01-22 17:00:36 -0300

    Gráficos das features variando com SNR e alguns errorbars

--- main.py
+++ main.py
@@ -11,37 +11,54 @@ number_of_frames = 100
 number_of_features = 9
 
 # Load the pickle file
-with open('\\data\\QPSK_ALL_SNR.pickle', 'rb') as handle:
+with open('C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\16QAM_ALL_SNR.pickle', 'rb') as handle:
     data = pickle.load(handle)
 
 # Quick code to separate frames
-signal = []
-a = 0
-b = frame_size
-for i in range(len(data) // frame_size):
-    signal.append(data[a:b])
-    a = a + frame_size
-    b = b + frame_size
+signal = np.empty([len(data), number_of_frames, frame_size, 2])
+for a in range(len(data)):
+    for b in range(number_of_frames):
+        signal[a, b, :, :] = data[a][b, :, :]
 
 # Parse frames
-frame = np.zeros((number_of_frames, frame_size), dtype=np.complex)
-for k in range(number_of_frames):
-    for i in range(frame_size):
-        frame[k][i] = (complex(signal[k][i][0], signal[k][i][1]))  # complex(Real, Imaginary)
+frame = np.zeros((len(data), number_of_frames, frame_size), dtype=np.complex)
+for snr in range(len(signal)):
+    for i in range(number_of_frames):
+        for j in range(frame_size):
+            frame[snr, i, j] = (complex(signal[snr, i, j, 0], signal[snr, i, j, 1]))  # complex(Real, Imaginary)
 
 # Plot frame
-plt.plot(frame[0][:].real)
-plt.plot(frame[0][:].imag)
+plt.plot(frame[5, 0, 0:200].real)
+plt.plot(frame[5, 0, 0:200].imag)
+plt.show()
+plt.plot(frame[15, 0, 0:200].real)
+plt.plot(frame[15, 0, 0:200].imag)
+plt.show()
+plt.plot(frame[25, 0, 0:200].real)
+plt.plot(frame[25, 0, 0:200].imag)
 plt.show()
 
 # Scatter plot
-plt.scatter(frame[0][:].real, frame[0][:].imag)
+plt.scatter(frame[25, 0, 0:200].real, frame[25, 0, 0:200].imag)
 plt.show()
 
 # Calculate features
-features = np.zeros((number_of_frames, number_of_features))
-for i in range(number_of_frames):
-    features[i][:] = ft.calculate_features(frame[i][:])
+features = np.zeros((len(frame), number_of_frames, number_of_features))
+for i in range(len(features)):
+    for j in range(number_of_frames):
+        features[i, j, :] = ft.calculate_features(frame[i, j, :])
+
+# Calculate mean of features
+mean_features = np.ndarray([len(frame), number_of_frames, number_of_features])
+std_features = np.ndarray([len(frame), number_of_frames, number_of_features])
+min_features = np.ndarray([len(frame), number_of_frames, number_of_features])
+max_features = np.ndarray([len(frame), number_of_frames, number_of_features])
+for i in range(len(mean_features)):
+    for j in range(number_of_features):
+        mean_features[i, :, j] = np.mean(features[i, :, j])
+        std_features[i, :, j] = np.std(features[i, :, j])
+        min_features[i, :, j] = np.min(features[i, :, j])
+        max_features[i, :, j] = np.max(features[i, :, j])
 
 # Example
 print('Desvio padrão do valor absoluto da componente não-linear da fase instantânea: ' + str(features[0, 0]))
@@ -54,42 +71,26 @@ print('Média da amplitude instantânea normalizada centralizada ao quadrado: ' 
 print('Desvio padrão do valor absoluto da amplitude instantânea normalizada e centralizada: ' + str(features[0, 7]))
 print('Desvio padrão da amplitude instantânea normalizada e centralizada: ' + str(features[0, 8]))
 
-plt.subplot(3, 3, 1)
-plt.plot(features[:, 0])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 2)
-plt.plot(features[:, 1])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 3)
-plt.plot(features[:, 2])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 4)
-plt.plot(features[:, 3])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 5)
-plt.plot(features[:, 4])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 6)
-plt.plot(features[:, 5])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 7)
-plt.plot(features[:, 6])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 8)
-plt.plot(features[:, 7])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.subplot(3, 3, 9)
-plt.plot(features[:, 8])
-plt.xlabel('Frame')
-plt.ylabel('Feature value')
-plt.show()
+var = np.linspace(-20, 30, 26)
+snr_array = np.zeros([len(frame), number_of_frames, number_of_features])
+for i in range(len(snr_array)):
+    for j in range(number_of_frames):
+        for k in range(number_of_features):
+            snr_array[i, j, k] = var[i]
 
-# TODO: error bar
+for n in range(number_of_features):
+    plt.subplot(1, 2, 1)
+    # plt.plot(snr_array[:, :, n], features[:, :, n])
+    plt.fill_between(snr_array[:, 0, 0],
+                     min_features[:, 0, n],
+                     max_features[:, 0, n],
+                     color='gray',
+                     alpha=0.2)
+    plt.xlabel('SNR')
+    plt.ylabel('Value')
+    plt.subplot(1, 2, 2)
+    # plt.plot(snr_array[:, :, n], features[:, :, n])
+    plt.errorbar(snr_array[:, 0, 0], mean_features[:, 0, n], yerr=3 * std_features[:, 0, n], uplims=True, lolims=True)
+    plt.xlabel('SNR')
+    plt.ylabel('Value')
+    plt.show()
