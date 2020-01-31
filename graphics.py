7ed65c6e85232cad9cc51c26abc14607c8e85b4b
Ronny Milléo <ronnysrmilleo@gmail.com>
2020-01-31 15:51:28 -0300

    Atualização para utilizar dataset do MATLAB

--- graphics.py
+++ graphics.py
@@ -7,12 +7,20 @@ import numpy as np
 frame_size = 1024
 number_of_frames = 256
 number_of_features = 9
-modulations = ['8PSK', '16PSK', '16QAM', '64QAM', '256QAM', 'BPSK', 'QPSK']
+modulations = ['BPSK', 'QPSK', '8PSK', '16QAM', '64QAM', '256QAM']
+
+selection = int(input('Press 1 to load features from PKL or any other number to load features from MAT: '))
 
 for modulation_number in range(len(modulations)):
-    pkl_file_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' + \
-                    modulations[modulation_number] + \
-                    '_features.pickle'
+    # Filename setup
+    if selection == 1:
+        pkl_file_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' + \
+                        modulations[modulation_number] + \
+                        '_features_from_PKL.pickle'
+    else:
+        pkl_file_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' + \
+                        modulations[modulation_number] + \
+                        '_features_from_MAT.pickle'
 
     # Load the pickle file
     with open(pkl_file_name, 'rb') as handle:
@@ -28,26 +36,7 @@ for modulation_number in range(len(modulations)):
             min_features[i, :, j] = np.min(features[i, :, j])
             max_features[i, :, j] = np.max(features[i, :, j])
 
-    # # Example
-    # print('Desvio padrão do valor abs da componente não-linear da fase instantânea: \n' +
-    #       str(mean_features[:, 0, 0]))
-    # print('Desvio padrão do valor direto da componente não-linear da fase instantânea: \n' +
-    #       str(mean_features[:, 0, 1]))
-    # print('Desvio padrão do valor abs da componente não-linear da freq instantânea: \n' +
-    #       str(mean_features[:, 0, 2]))
-    # print('Desvio padrão do valor direto da componente não-linear da freq instantânea: \n' +
-    #       str(mean_features[:, 0, 3]))
-    # print('Curtose: \n' +
-    #       str(mean_features[:, 0, 4]))
-    # print('Valor máximo da DEP da amplitude instantânea normalizada e centralizada: \n' +
-    #       str(mean_features[:, 0, 5]))
-    # print('Média da amplitude instantânea normalizada centralizada ao quadrado: \n' +
-    #       str(mean_features[:, 0, 6]))
-    # print('Desvio padrão do valor abs da amplitude instantânea normalizada e centralizada: \n' +
-    #       str(mean_features[:, 0, 7]))
-    # print('Desvio padrão da amplitude instantânea normalizada e centralizada: \n' +
-    #       str(mean_features[:, 0, 8]))
-
+    # SNR axis setup
     var = np.linspace(-20, 30, 26)
     snr_array = np.zeros([len(features), number_of_frames, number_of_features])
     for i in range(len(snr_array)):
@@ -55,6 +44,7 @@ for modulation_number in range(len(modulations)):
             for k in range(number_of_features):
                 snr_array[i, j, k] = var[i]
 
+    # Plot graphics
     for n in range(number_of_features):
         plt.figure(num=n, figsize=(6.4, 3.6), dpi=300)
         if n == 5:
@@ -77,9 +67,19 @@ for modulation_number in range(len(modulations)):
         plt.title('Feature ' + str(n + 1))
         plt.legend(modulations)
 
-for n in range(number_of_features):
-    plt.figure(num=n)
-    figure_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\figures\\all_modulations_feature_' + \
-                  str(n) + \
-                  '.png'
-    plt.savefig(figure_name, figsize=(6.4, 3.6), dpi=300)
+if selection == 1:
+    # Save figures
+    for n in range(number_of_features):
+        plt.figure(num=n)
+        figure_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\figures\\features_from_PKL' + \
+                      str(n) + \
+                      '.png'
+        plt.savefig(figure_name, figsize=(6.4, 3.6), dpi=300)
+else:
+    # Save figures
+    for n in range(number_of_features):
+        plt.figure(num=n)
+        figure_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\figures\\features_from_MAT' + \
+                      str(n) + \
+                      '.png'
+        plt.savefig(figure_name, figsize=(6.4, 3.6), dpi=300)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pickle
 
 import numpy as np
+import scipy.io
 
 import features as ft
 
@@ -9,42 +10,85 @@ frame_size = 1024
 number_of_frames = 4096
 number_of_snr = 26
 number_of_features = 9
-modulations = ['BPSK', 'QPSK', 'OQPSK', '8PSK', '16PSK', '32PSK', '16QAM', '32QAM', '64QAM', '128QAM', '256QAM']
+modulations = ['BPSK', 'QPSK', '8PSK', '16QAM', '64QAM', '256QAM']
+
+selection = int(input('Press 1 to load raw PKL or any other number to load raw MAT: '))
 
 for modulation_number in range(len(modulations)):
-    pkl_file_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' + \
-                    modulations[modulation_number] + \
-                    '_RAW.pickle'
+    if selection == 1:
+        # Filename setup
+        pkl_file_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' + \
+                        modulations[modulation_number] + \
+                        '_RAW.pickle'
 
-    # Load the pickle file
-    with open(pkl_file_name, 'rb') as handle:
-        data = pickle.load(handle)
+        # Load the pickle file
+        with open(pkl_file_name, 'rb') as handle:
+            data = pickle.load(handle)
+        print(pkl_file_name + ' file loaded...')
 
-    # Quick code to separate SNR
-    start = 0
-    end = 4096
-    signal = np.empty([number_of_snr, number_of_frames, frame_size, 2])
-    for snr in range(number_of_snr):
-        signal[snr, 0:4096, :, :] = data[0][start:end, :, :]
-        start += 4096
-        end += 4096
+        # Quick code to separate SNR
+        start = 0
+        end = 4096
+        signal = np.empty([number_of_snr, number_of_frames, frame_size, 2])
+        for snr in range(number_of_snr):
+            signal[snr, 0:4096, :, :] = data[0][start:end, :, :]
+            start += 4096
+            end += 4096
+        print('Signal split in different SNR...')
 
-    # Parse signal
-    parsed_signal = np.zeros((number_of_snr, number_of_frames, frame_size), dtype=np.complex)
-    for snr in range(number_of_snr):
-        for frames in range(number_of_frames):
-            for samples in range(frame_size):
-                parsed_signal[snr, frames, samples] = (complex(signal[snr, frames, samples, 0],
-                                                               signal[snr, frames, samples, 1]))
+        # Parse signal
+        parsed_signal = np.zeros((number_of_snr, number_of_frames, frame_size), dtype=np.complex)
+        for snr in range(number_of_snr):
+            for frames in range(number_of_frames):
+                for samples in range(frame_size):
+                    parsed_signal[snr, frames, samples] = (complex(signal[snr, frames, samples, 0],
+                                                                   signal[snr, frames, samples, 1]))
+        print('Signal parsed...')
 
-    # Calculate features
-    features = np.zeros((number_of_snr, number_of_frames, number_of_features))
-    for snr in range(number_of_snr):
-        for frames in range(number_of_frames):
-            features[snr, frames, :] = ft.calculate_features(parsed_signal[snr, frames, :])
+        # Calculate features
+        features = np.zeros((number_of_snr, number_of_frames, number_of_features))
+        for snr in range(number_of_snr):
+            for frames in range(number_of_frames):
+                features[snr, frames, :] = ft.calculate_features(parsed_signal[snr, frames, :])
+        print('Features calculated...')
 
-    # Save the samples ina pickle file
-    with open('C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' +
-              modulations[modulation_number] +
-              '_features.pickle', 'wb') as handle:
-        pickle.dump(features, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        # Save the samples ina pickle file
+        with open('C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' +
+                  modulations[modulation_number] +
+                  '_features_from_PKL.pickle', 'wb') as handle:
+            pickle.dump(features, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print('File saved...')
+        print('Finished.')
+    else:
+        # Filename setup
+        mat_file_name = 'C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' + \
+                        modulations[modulation_number] + \
+                        '_RAW.mat'
+
+        # Dictionary to access variable inside MAT file
+        info = {'BPSK': 'pks2_signal',
+                'QPSK': 'pks4_signal',
+                '8PSK': 'pks8_signal',
+                '16QAM': 'qam16_signal',
+                '64QAM': 'qam64_signal',
+                '256QAM': 'qam256_signal'}
+
+        # Load MAT file
+        data_mat = scipy.io.loadmat(mat_file_name)
+        print(mat_file_name + ' file loaded and already parsed...')
+        parsed_signal = data_mat[info[modulations[modulation_number]]]
+
+        # Calculate features
+        features = np.zeros((number_of_snr, number_of_frames, number_of_features))
+        for snr in range(number_of_snr):
+            for frames in range(number_of_frames):
+                features[snr, frames, :] = ft.calculate_features(parsed_signal[snr, frames, :])
+        print('Features calculated...')
+
+        # Save the samples ina pickle file
+        with open('C:\\Users\\ronny\\PycharmProjects\\amcpy\\data\\' +
+                  modulations[modulation_number] +
+                  '_features_from_MAT.pickle', 'wb') as handle:
+            pickle.dump(features, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print('File saved...')
+        print('Finished.')
