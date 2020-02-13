5532a7cad7d8c266ff4a01dfbd9fa961ec224054
Ronny Milléo <ronnysrmilleo@gmail.com>
2020-02-13 18:28:33 -0300

    Leitura dos arquivos binários do GR

--- read_binary_stream.py
+++ read_binary_stream.py
@@ -1,10 +1,32 @@
-import numpy as np
+import os
+import pathlib
+import pickle
+from os.path import join
+
 import matplotlib.pyplot as plt
+import numpy as np
 
-file_name = "/home/gics-administrador/Documentos/file"
+modulations = ['BPSK', 'QPSK', 'PSK8', 'QAM16']
+initial_delay = 256
+number_of_samples = 1024
 
-# with open(file_name, "rb", buffering=0) as handle:
+# Plot to verify modulation signals
+for modulation in modulations:
+    file_name = pathlib.Path(join(os.getcwd(), 'gr-data', 'binary_' + modulation))
+    # Complex64 because it's float32 on I and Q
+    data = np.fromfile(file_name, dtype=np.complex64)
+    plt.figure(num=1)
+    plt.plot(np.real(data[initial_delay:number_of_samples + initial_delay]))
+    plt.plot(np.imag(data[initial_delay:number_of_samples + initial_delay]))
+    plt.show()
+    plt.figure(num=2)
+    plt.scatter(np.real(data[initial_delay:number_of_samples + initial_delay]),
+                np.imag(data[initial_delay:number_of_samples + initial_delay]))
+    plt.show()
 
-data = np.fromfile(file_name, dtype=complex)
+    # Save binary files in pickle (without delay)
+    with open(pathlib.Path(join(os.getcwd(), 'gr-data', modulation + '.pickle')), 'wb') as handle:
+        pickle.dump(data[1:len(data)], handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print(modulation + ' file saved...')
 
-plt.plot(data[1:1000])
+print('Finished.')
