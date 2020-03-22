ca8298f2508314f4142d713b673e36e26e4eded2
Ronny Milléo <ronnysrmilleo@gmail.com>
2020-03-22 15:53:50 -0300

    Desvio padrão nos plots de erro; Reescrita do main reduzindo código, otimizando e comentando tudo.

--- graphics.py
+++ graphics.py
@@ -7,8 +7,10 @@ from os.path import join
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Open json file with parameters
 with open("./info.json") as handle:
     infoJson = json.load(handle)
+
 # Config
 frame_size = infoJson['frameSize']
 number_of_modulations = len(infoJson['modulations']['index'])
@@ -21,7 +23,10 @@ feature_names = infoJson['features']['names']
 features = []
 for modulation in range(number_of_modulations):
     # Filename setup
-    pkl_file_name = pathlib.Path(join(os.getcwd(), 'gr-data', 'pickle', modulation_names[modulation] + '_features.pickle'))
+    pkl_file_name = pathlib.Path(join(os.getcwd(),
+                                      'gr-data',
+                                      'pickle',
+                                      modulation_names[modulation] + '_features.pickle'))
 
     # Load the pickle file
     with open(pkl_file_name, 'rb') as handle:
@@ -29,14 +34,12 @@ for modulation in range(number_of_modulations):
 
 # Calculate mean, min and max of features by SNR and by modulation
 mean_features = np.ndarray([number_of_modulations, number_of_snr, number_of_frames, number_of_features])
-min_features = np.ndarray([number_of_modulations, number_of_snr, number_of_frames, number_of_features])
-max_features = np.ndarray([number_of_modulations, number_of_snr, number_of_frames, number_of_features])
+std_features = np.ndarray([number_of_modulations, number_of_snr, number_of_frames, number_of_features])
 for m in range(number_of_modulations):
     for snr in range(number_of_snr):
         for ft in range(number_of_features):
             mean_features[m, snr, :, ft] = np.mean(features[m][snr, :, ft])
-            min_features[m, snr, :, ft] = np.min(features[m][snr, :, ft])
-            max_features[m, snr, :, ft] = np.max(features[m][snr, :, ft])
+            std_features[m, snr, :, ft] = np.std(features[m][snr, :, ft])
 
 # SNR axis setup
 var = np.linspace(-20, 20, 21)
@@ -50,54 +53,39 @@ for m in range(number_of_modulations):
 # Plot graphics using only mean
 for n in range(number_of_features):
     plt.figure(num=n, figsize=(6.4, 3.6), dpi=300)
-    plt.plot(snr_array[0, :, n, n], mean_features[0, :, n, n], linewidth=1.0)
-    plt.plot(snr_array[1, :, n, n], mean_features[1, :, n, n], linewidth=1.0)
-    plt.plot(snr_array[2, :, n, n], mean_features[2, :, n, n], linewidth=1.0)
-    plt.plot(snr_array[3, :, n, n], mean_features[3, :, n, n], linewidth=1.0)
+    for m in range(number_of_modulations):
+        plt.plot(snr_array[m, :, n, n], mean_features[m, :, n, n], linewidth=1.0)
+    plt.title('Feature ' + str(n + 1) + ' - ' + feature_names[n])
     plt.xlabel('SNR')
     plt.ylabel('Value')
-    plt.title('Feature ' + str(n + 1) + ' - ' + feature_names[n])
     plt.legend(modulation_names)
     figure_name = pathlib.Path(join(os.getcwd(), 'figures', 'features', 'feature_' + str(n + 1) + '.png'))
     plt.savefig(figure_name, figsize=(6.4, 3.6), dpi=300)
     plt.close()
 
-# Plot graphics with all frames (actually, from 0 to 100 to make it more readable and fast)
+# Plot graphics with all frames
 for n in range(number_of_features):
     plt.figure(num=n, figsize=(6.4, 3.6), dpi=300)
-    plt.plot(snr_array[0, :, 0:100, n], features[0][:, 0:100, n], 'b', linewidth=1.0)
-    plt.plot(snr_array[1, :, 0:100, n], features[1][:, 0:100, n], 'r', linewidth=1.0)
-    plt.plot(snr_array[2, :, 0:100, n], features[2][:, 0:100, n], 'c', linewidth=1.0)
-    plt.plot(snr_array[3, :, 0:100, n], features[3][:, 0:100, n], 'm', linewidth=1.0)
+    for m in range(number_of_modulations):
+        plt.plot(snr_array[m, :, :, n], mean_features[m, :, :, n], linewidth=1.0)
     plt.xlabel('SNR')
     plt.ylabel('Value')
     plt.title('Feature ' + str(n + 1) + ' - ' + feature_names[n])
+    # TODO: put modulation names in legend
     # plt.legend(modulation_names)
     figure_name = pathlib.Path(join(os.getcwd(), 'figures', 'features', 'feature_' + str(n + 1) + '_all_frames.png'))
     plt.savefig(figure_name, figsize=(6.4, 3.6), dpi=300)
     plt.close()
 
-# Plot graphics with error bar using min and max of every feature by SNR
+# Plot graphics with error bar using standard deviation
 for n in range(number_of_features):
     plt.figure(num=n, figsize=(6.4, 3.6), dpi=300)
-    plt.errorbar(snr_array[0, :, n, n],
-                 mean_features[0, :, n, n],
-                 yerr=[mean_features[0, :, n, n] - min_features[1, :, n, n],
-                       max_features[0, :, n, n] - mean_features[1, :, n, n]])
-    plt.errorbar(snr_array[1, :, n, n],
-                 mean_features[1, :, n, n],
-                 yerr=[mean_features[1, :, n, n] - min_features[1, :, n, n],
-                       max_features[1, :, n, n] - mean_features[1, :, n, n]])
-    plt.errorbar(snr_array[2, :, n, n],
-                 mean_features[2, :, n, n],
-                 yerr=[mean_features[2, :, n, n] - min_features[1, :, n, n],
-                       max_features[2, :, n, n] - mean_features[1, :, n, n]])
-    plt.errorbar(snr_array[3, :, n, n],
-                 mean_features[3, :, n, n],
-                 yerr=[mean_features[3, :, n, n] - min_features[1, :, n, n],
-                       max_features[3, :, n, n] - mean_features[1, :, n, n]])
+    for m in range(number_of_modulations):
+        plt.errorbar(snr_array[m, :, n, n],
+                     mean_features[m, :, n, n],
+                     yerr=std_features[m, :, n, n])
     plt.xlabel('SNR')
-    plt.ylabel('Value')
+    plt.ylabel('Value with sigma')
     plt.title('Feature ' + str(n + 1) + ' - ' + feature_names[n])
     plt.legend(modulation_names)
     figure_name = pathlib.Path(join(os.getcwd(), 'figures', 'features', 'feature_' + str(n + 1) + '_error_bar.png'))
